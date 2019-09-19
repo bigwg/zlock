aa713f6f4570959198ba0397485ce9f38eb5cc3f
zhaozhiwei <zhaozw@missfresh.cn>
2019-09-19 15:51:26 +0800

    alter RedisSourceTest class

--- zlock-core/src/test/java/com/zzw/distribution/lock/core/source/RedisSourceTest.java
+++ zlock-core/src/test/java/com/zzw/distribution/lock/core/source/RedisSourceTest.java
@@ -16,6 +16,8 @@ public class RedisSourceTest {
 
     private Source source;
 
+    private String lockName = "zhaozhiweiTryAcquireLock";
+
     @BeforeAll
     public void beforeAll(){
         source = new RedisSource("10.12.0.8", 6379, "MBkMl4cssBcbet1W");
@@ -23,8 +25,15 @@ public class RedisSourceTest {
 
     @Test
     public void tryAcquireTest() throws InterruptedException {
-        boolean result = source.tryAcquire("zhaozhiweiTryAcquireLock", 1);
+        boolean result = source.tryAcquire(lockName, 1);
         Assertions.assertTrue(result);
         Thread.sleep(20000);
+        tryReleaseTest();
+    }
+
+    @Test
+    public void tryReleaseTest(){
+        boolean release = source.release(lockName, 1);
+        Assertions.assertTrue(release);
     }
 }
